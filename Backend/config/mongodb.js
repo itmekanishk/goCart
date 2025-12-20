58281d6056296ce136200cd88c076f0a77ee7ded
Kanishk <7705kanishkkumar@gmailcom>
2025-12-20 18:20:57 +0530

    updated mongoDD config

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 
 
+let cachedConnection = null;
 
 const connectDB = async () => {
+    // ADD THESE 3 LINES
+    if (cachedConnection && mongoose.connection.readyState === 1) {
+        return cachedConnection;
+    }
+
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/e-commerce`,
+            {
+                
+                serverSelectionTimeoutMS: 5000,
+                socketTimeoutMS: 45000,
+            }
+        )
+        
+        cachedConnection = connectionInstance; 
+        
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
+        
+        throw error;
     }
 }
 
